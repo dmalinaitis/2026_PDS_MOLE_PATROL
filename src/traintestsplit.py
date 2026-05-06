f0b71f04dd38ce7071d557b9d4f44410141b8059
Jamie Underwood <260853765+JamieUnderwoo@users.noreply.github.com>
2026-05-06 13:40:24 +0200

    Added a new file validation.csv. This file is a manual split of the dataset for validation use only. This was done manually due to a lack of support for stratification and group k folding in sklearn. Traintestsplit function file reworked to account for this change.

--- src/traintestsplit.py
+++ src/traintestsplit.py
@@ -1,6 +1,13 @@
 from sklearn.model_selection import StratifiedGroupKFold
 import pandas as pd
 import os
+def cancer(x):
+  '''Function to change class labels to binary value of C and NC. '''
+  ctype = ['SCC','BCC','MEL']
+  if x in ctype:
+    return('C')
+  else:
+    return('NC')
 def spliting():
     '''Function that uses the metadata file of a given dataset, takes the image and patient ids with the lesion type and uses them 
     make a cross validation split using stratified group k folding.'''
@@ -8,6 +15,11 @@ def spliting():
     os.chdir(os.path.dirname(currentdir))
     #changing working directory to ensure easy access to metadata for split
     df = pd.read_csv('./data/metadata.csv')
+    valdf = pd.read_csv('./data/Validation.csv')
+    #validation handleing, due to bin sorting problem this had to be done by hand.
+    valpat = valdf['patient_id']
+    valpatlist = valpat.to_list()
+    df = df[~df['patient_id'].isin(valpatlist)]
     group = df['patient_id']
     y = df['diagnostic']
     x = df['img_id']
@@ -25,4 +37,4 @@ def spliting():
         traingrouparray[i] = group[train_index]
         testindexarray[i] = test_index
         testgrouparray[i] = group[test_index]
-    return trainindexarray, traingrouparray, testindexarray, testgrouparray
+    return trainindexarray, traingrouparray, testindexarray, testgrouparray, valpatlist
